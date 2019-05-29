d16f60b7b079f95ee87c7eb08771270f007d0d27
Aditya Acharya <aacha002@ucr.edu>
2019-05-29 16:12:14 -0700

    Database stuff

--- app.py
+++ app.py
@@ -1,14 +1,44 @@
 from flask import Flask, render_template, request
-from flask_sqlalchemy import SQLAlchemy
+import psycopg2
+import sys
+#from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-
-db = SQLAlchemy(app)
+con = None
 
 
 
 @app.route("/")
 def main():
+
+
+
+
+
+
+        try: 
+            con = psycopg2.connect("host = 'localhost' dbname = 'testdb' user = 'adialachar' password = 'squirtle123'")
+            cur = con.cursor()
+
+            #cur.execute(SQL statement)
+
+
+
+            con.commit()
+
+        except psycopg2.DatabaseError, e:
+            if con:
+                con.rollback()
+            print("Error")
+            sys.exit(1)
+
+
+
+        finally:
+            if con:
+                con.close()
+
+
 	return render_template("index.html")
 
 
