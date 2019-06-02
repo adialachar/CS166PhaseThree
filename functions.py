af61d98b4c6debb985371a71670fae8f0b5a0b6b
Aditya Acharya <30582104+adialachar@users.noreply.github.com>
2019-06-01 18:04:45 -0700

    Update functions.py

--- functions.py
+++ functions.py
@@ -7,9 +7,38 @@ con = None
 
 
 def checkPilot(pilot_name,pilotstuff):
+    
+    try:
+            con = psycopg2.connect("host = 'localhost' dbname = 'testdb' user = 'adialachar' password = 'squirtle123'")
+            cur = con.cursor()
 
 
-    return 0    
+
+            cur.execute("SELECT COUNT(*) FROM Reservation R WHERE R.fid = %s AND R._status = %s;"())
+
+            while True:
+                row = cur.fetchone()
+
+                if row == None:
+                    break
+
+                print("{0}".format(row[0]))
+
+
+
+        except psycopg2.DatabaseError as e:
+            if con:
+                con.rollback()
+            print("Error")
+            print(e)
+            sys.exit(1)
+
+
+
+        finally:
+            if con:
+                con.close()
+
 
 
 
@@ -27,4 +56,64 @@ def checkPilot(pilot_name,pilotstuff):
 
 def checkPlane():
 
+    if request.method == 'POST':
+
+
+        try:
+            con = psycopg2.connect("host = 'localhost' dbname = 'testdb' user = 'adialachar' password = 'squirtle123'")
+            cur = con.cursor()
+
+
+
+            cur.execute("SELECT COUNT(*) FROM Reservation R WHERE R.fid = %s AND R._status = %s;"())
+
+            while True:
+                row = cur.fetchone()
+
+                if row == None:
+                    break
+
+                print("{0}".format(row[0]))
+
+
+
+        except psycopg2.DatabaseError as e:
+            if con:
+                con.rollback()
+            print("Error")
+            print(e)
+            sys.exit(1)
+
+
+
+        finally:
+            if con:
+                con.close()
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     return 0
